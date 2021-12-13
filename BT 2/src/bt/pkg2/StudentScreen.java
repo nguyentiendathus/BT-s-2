44309fb38e1b6cb75fd5374b2396bd17dbc4946a
KyoGren <HI@192.168.1.9>
2021-12-13 21:42:09 +0700

    Add Student class

--- BT 2/src/bt/pkg2/StudentScreen.java
+++ BT 2/src/bt/pkg2/StudentScreen.java
@@ -9,6 +9,7 @@ public class StudentScreen extends javax.swing.JFrame {
         spacePanel.setVisible(true);
     }
 
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
